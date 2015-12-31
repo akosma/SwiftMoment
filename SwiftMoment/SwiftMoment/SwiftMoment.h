f4839f46535b4492636512d19a58f8d43dcaa2b4
Madhava Jay <me@madhavajay.com>
2015-12-31 16:57:15 +0100

    Replaced UIKit with Foundation

--- SwiftMoment/SwiftMoment/SwiftMoment.h
+++ SwiftMoment/SwiftMoment/SwiftMoment.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Adrian Kosmaczewski. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftMoment.
 FOUNDATION_EXPORT double SwiftMomentVersionNumber;

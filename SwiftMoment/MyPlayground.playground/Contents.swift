37119b08efec0a36226106ac9ea6cfbf8f6d290a
Adrian Kosmaczewski <akosma@me.com>
2016-10-12 17:04:10 +0200

    Updated Playground

--- SwiftMoment/MyPlayground.playground/Contents.swift
+++ SwiftMoment/MyPlayground.playground/Contents.swift
@@ -11,4 +11,11 @@ obj.fromNow()
 
 let today = moment()
 let first = moment(today).add(50.days)
-let second = moment(today).add(50, .Days)
+let second = moment(today).add(50, .days)
+
+let meierskappel = moment("1847-11-23")!
+let gettysburg = moment("1863-07-1")!
+let pavon = moment("1861-09-17")!
+let moments = [pavon, gettysburg, meierskappel]
+let sorted = moments.sorted(by: <)
+print(sorted)

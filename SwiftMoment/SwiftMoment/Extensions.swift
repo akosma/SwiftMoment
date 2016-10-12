bb4b29d048c851b5f49280fcfd9b5093e080f1c5
Adrian Kosmaczewski <akosma@me.com>
2016-10-12 13:05:47 +0200

    Documented the public extension

--- SwiftMoment/SwiftMoment/Extensions.swift
+++ SwiftMoment/SwiftMoment/Extensions.swift
@@ -7,34 +7,43 @@
 //
 
 public extension Int {
+
+    /// Creates a duration value with the current number of seconds.
     var seconds: Duration {
         return Duration(value: self)
     }
 
+    /// Creates a duration value with the current number of minutes.
     var minutes: Duration {
         return Duration(value: self * 60)
     }
 
+    /// Creates a duration value with the current number of hours.
     var hours: Duration {
         return Duration(value: self * 3600)
     }
 
+    /// Creates a duration value with the current number of weeks.
     var weeks: Duration {
         return Duration(value: self * 604800)
     }
 
+    /// Creates a duration value with the current number of days.
     var days: Duration {
         return Duration(value: self * 86400)
     }
 
+    /// Creates a duration value with the current number of months.
     var months: Duration {
         return Duration(value: self * 2592000)
     }
 
+    /// Creates a duration value with the current number of quarters.
     var quarters: Duration {
         return Duration(value: self * 7776000)
     }
 
+    /// Creates a duration value with the current number of years.
     var years: Duration {
         return Duration(value: self * 31536000)
     }
